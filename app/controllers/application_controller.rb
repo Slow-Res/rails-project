5b29c08b1e13953246a5ce6904a708ff667a5433
Abdelrahman Alasal <>
2023-11-15 13:59:57 +0300

    Init controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,34 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    protect_from_forgery with: :exception
+
+
+
+    def after_sign_in_path_for(resource)
+        if resource.is_a?(User) && resource.role == "admin"
+            # Redirect admin to a specific path
+            admin_dashboard_path
+        elsif resource.is_a?(User) && resource.role == "teacher"
+            # Redirect teacher to a specific path
+            teacher_dashboard_path
+        elsif resource.is_a?(User) && resource.role == "student"
+            # Redirect student to a specific path
+            student_dashboard_path
+        else
+            # Redirect other users to a default path
+            super
+        end
+    end
+
+ 
+
+    protected
+  
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :date_of_birth, :phone_number, :address, :city, :country, :role])
+    end
+
+
+
 end
---
+++ app/controllers/student_dashboard_controller.rb
@@ -0,0 +1,7 @@
+class StudentDashboardController < ApplicationController
+
+    def index
+
+    end
+    
+end
